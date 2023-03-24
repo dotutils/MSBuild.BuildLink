3b0e0b3e2006aaa42053f4a996be4a609ea34654
Jan Krivanek <jankrivanek@microsoft.com>
2023-03-24 09:16:01 +0100

    Parse kusto stats

---
+++ prototypes/NugetExperiments/Runner/NugetStats/StatsParser.cs
@@ -0,0 +1,101 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace Runner.NugetStats
+{
+    internal static class StatsParser
+    {
+        public const string StatsPath = @"NugetStats\packages-stats.csv";
+
+        public static IEnumerable<NugetStatsRecord> FetchTopStats(string path = StatsPath)
+        {
+            using var reader = new StreamReader(path);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            csv.Context.RegisterClassMap<NugetStatsRecordMap>();
+            foreach (var record in csv.GetRecords<NugetStatsRecord>())
+            {
+                // exclude '[]'
+                if ((record.SourceLinkResult == SourceLinkResult.ValidExternal || record.SourceLinkResult == SourceLinkResult.Valid)
+                    &&
+                    !string.IsNullOrEmpty(record.SourceUrlRepoInfo) && record.SourceUrlRepoInfo.Length > 2)
+                {
+                    var arr = JArray.Parse(record.SourceUrlRepoInfo);
+                    //var obj = JObject.Parse(record.SourceUrlRepoInfo);
+                    if ((arr.First as JObject).Count != 3)
+                    {
+                        throw new Exception("Unexpected data");
+                    }
+
+                    //Repo, FileCount, Example
+                    JProperty repoProp = (arr.First as JObject).Properties().First(p => p.Name == "Repo");
+                    string repoType = GetChildPropertyValue(repoProp, "Type");
+                    if (repoType != "GitHub")
+                    {
+                        throw new Exception("Unsupported repo type: " + repoType);
+                    }
+                    string repoRef = GetChildPropertyValue(repoProp, "Ref");
+                    string owner = GetChildPropertyValue(repoProp, "Owner");
+                    string repoName = GetChildPropertyValue(repoProp, "Repo");
+
+                    record.RemoteRef = repoRef;
+                    record.RemoteUrl = $"github.com/{owner}/{repoName}.git";
+                }
+                yield return record;
+            }
+        }
+
+        private static string GetChildPropertyValue(JProperty property, string childPropName)
+        {
+            return property.Values().Cast<JProperty>().First(v => v.Name == childPropName).Value.ToString();
+        }
+
+        public sealed class NugetStatsRecordMap : ClassMap<NugetStatsRecord>
+        {
+            public NugetStatsRecordMap()
+            {
+                AutoMap(CultureInfo.InvariantCulture);
+                Map(m => m.RemoteRef).Ignore();
+                Map(m => m.RemoteUrl).Ignore();
+            }
+        }
+    }
+
+    public class NugetStatsRecord
+    {
+        public string Id { get; set; }
+        public SourceLinkResult SourceLinkResult { get; set; }
+        public CompilerFlagsResult CompilerFlagsResult { get; set; }
+        public string SourceUrlRepoInfo { get; set; }
+        public string RemoteUrl { get; set; }
+        public string RemoteRef { get; set; }
+    }
+
+    public enum SourceLinkResult
+    {
+        HasUntrackedSources, //The package has valid SourceLink but has untracked sources
+        InvalidSourceLink, //The package has invalid SourceLink metadata
+        NoSourceLink, //The package has missing SourceLink metadata
+        NoSymbols, //The package has missing symbols
+        NothingToValidate, //The package has no assemblies to validate
+        Valid, //The package has valid embedded symbols
+        ValidExternal, //The package has valid externally hosted symbols
+    }
+
+    public enum CompilerFlagsResult
+    {
+        Missing, //The package symbols don't have compiler flags
+        NothingToValidate, //The package has no assemblies to validate
+        Present, //The package symbols have compiler flags but are too old to be reproducible
+        Valid, //The package symbols have compiler flags and are reproducible
+    }
+}
--- prototypes/NugetExperiments/Runner/Program.cs
+++ prototypes/NugetExperiments/Runner/Program.cs
@@ -2,17 +2,26 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using NugetUtils;
+using Runner.NugetStats;
 
 namespace Runner
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
+        {
+            foreach (NugetStatsRecord nugetStatsRecord in StatsParser.FetchTopStats())
+            {
+                Console.WriteLine(nugetStatsRecord.RemoteUrl);
+            }
+        }
+
+        static async void FetchTopStatsFromNugetOrg()
         {
             Console.WriteLine("Hello, World!");
             //await new NugetStats().AsyncMethod();
 
-            NugetStats nugetStats = new();
+            NugetUtils.NugetStats nugetStats = new();
             await nugetStats.WaitForInitialization().ConfigureAwait(false);
             foreach (PackageStatsInfo nugetStatsCommunityPackage in nugetStats.CommunityPackages)
             {
