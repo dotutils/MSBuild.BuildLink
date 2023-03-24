1ca5fdc7a75f1fb2d2401f3e8f4c3a150b541ff8
Jan Krivanek <jankrivanek@microsoft.com>
2023-03-24 20:27:04 +0100

    Add codes fetching

---
+++ prototypes/NugetExperiments/NugetUtils/SourceFetcher.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+
+namespace NugetUtils
+{
+    public class SourceFetcher
+    {
+        private readonly string _sourcesRoot;
+
+        public SourceFetcher(string sourcesRoot = "src") => _sourcesRoot = sourcesRoot;
+
+        public string FetchRepo(string remote, string revision)
+        {
+            remote = "https://" + remote;
+            string localPath = Path.Combine(_sourcesRoot, GetLocalPath(remote));
+            string repoPath;
+            if (!Path.Exists(localPath))
+            {
+                repoPath = Repository.Clone(remote, Path.Combine(_sourcesRoot, GetLocalPath(remote)),
+                    new CloneOptions()
+                    {
+                        //BranchName = revision,
+                        Checkout = false, RecurseSubmodules = true,
+                    });
+            }
+            else
+            {
+                repoPath = Path.Combine(localPath, ".git");
+            }
+
+            using var repo = new Repository(repoPath);
+            Commands.Checkout(repo, revision);
+            return localPath;
+        }
+
+        //"https://github.com/libgit2/libgit2sharp.git"
+        private static string GetLocalPath(string remote)
+        {
+            if (remote.EndsWith(".git", StringComparison.InvariantCultureIgnoreCase))
+            {
+                remote = remote.Substring(0, remote.Length - ".git".Length);
+            }
+
+            return string.Join('-', remote.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries).TakeLast(2));
+        }
+    }
+}
--- prototypes/NugetExperiments/Runner/Program.cs
+++ prototypes/NugetExperiments/Runner/Program.cs
@@ -10,10 +10,22 @@ namespace Runner
     {
         static void Main(string[] args)
         {
+            FetchCode();
+            return;
+
             foreach (NugetStatsRecord nugetStatsRecord in StatsParser.FetchTopStats())
             {
-                Console.WriteLine(nugetStatsRecord.RemoteUrl);
+                Console.WriteLine(nugetStatsRecord.RemoteUrl + "    " + nugetStatsRecord.RemoteRef);
             }
+        }
+
+        static void FetchCode()
+        {
+            //github.com/JamesNK/Newtonsoft.Json.git    0a2e291c0d9c0c7675d445703e51750363a549ef
+
+            SourceFetcher sf  = new SourceFetcher();
+            string res = sf.FetchRepo("github.com/JamesNK/Newtonsoft.Json.git", "0a2e291c0d9c0c7675d445703e51750363a549ef");
+            Console.WriteLine(res);
         }
 
         static async void FetchTopStatsFromNugetOrg()
